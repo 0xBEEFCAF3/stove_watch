d350c8e1ddf7043ec898d3d34a80ea719e969da2
Tuna Sogut <sogutt@bu.edu>
2018-12-04 13:32:40 -0500

    temperaure code done

---
+++ temp_sensor/temp_sensor.ino
@@ -0,0 +1,31 @@
+
+float temp;
+int tempPin = 2; // analog input pin
+int sampleTime = 1000; // 1 second dafault 
+
+void setup() 
+{
+ Serial.begin(9600);
+}
+
+void loop() {
+
+  temp = analogRead(tempPin);
+  Serial.print("RAW DATA: ");
+  Serial.print (temp);
+  Serial.println(" ");
+  //converts raw data into degrees celsius and prints it out
+  // 500mV/1024=.48828125
+  temp = temp * 0.48828125;
+  Serial.print("CELSIUS: ");
+  Serial.print(temp);
+  Serial.println("*C ");
+  //converts celsius into fahrenheit 
+  temp = temp *9 / 5;
+  temp = temp + 32;
+  Serial.print("FAHRENHEIT: ");
+  Serial.print(temp);
+  Serial.println("*F");
+  delay(sampleTime);
+
+}
