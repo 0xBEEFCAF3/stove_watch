e6c3f27beca5675a010758a7ccfb4c13e40ac89c
Armin Sabouri <armins@mit.edu>
2018-12-19 23:02:54 -0500

    format sensor readings as JSON

--- temp_gas_sensors/temp_gas_sensors.ino
+++ temp_gas_sensors/temp_gas_sensors.ino
@@ -9,14 +9,20 @@ void setup()
 }
 
 void loop() {
+  float cur_temp;
+  float cur_gas;
+  String JSON_object;
 
-  get_temp();
-  get_gas(
-  Serial.println(analogRead(gas_sensor));
-  delay(sample_time);
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    //incomingByte = Serial.read();
+    cur_temp = get_temp();
+    cur_gas = get_gas();
+    JSON_object = "{'temp':"+(String)cur_temp+",'gas':"+(String)cur_gas+"}";
+    Serial.println(JSON_object);
+  }
+    delay(sample_time);    
 }
-
-
 
 float get_gas(){
   int i;
@@ -27,11 +33,11 @@ float get_gas(){
   gas_value = gas_value/50;
   // Serial.print("Gas Value: ");
   // Serial.println(gas_value);
-  return gas_value
+  return gas_value;
 }
 
 
-double get_temp(){
+float get_temp(){
   temp = analogRead(temp_sensor);
   //Serial.print("RAW DATA: ");
   //Serial.print (temp);
@@ -39,14 +45,16 @@ double get_temp(){
   //converts raw data into degrees celsius and prints it out
   // 500mV/1024=.48828125
   temp = temp * 0.48828125;
-  Serial.print("CELSIUS: ");
-  Serial.print(temp);
-  Serial.println("*C ");
+  // Serial.print("CELSIUS: ");
+  // Serial.print(temp);
+  // Serial.println("*C ");
   //converts celsius into fahrenheit 
   temp = temp *9 / 5;
   temp = temp + 32;
-  Serial.print("FAHRENHEIT: ");
-  Serial.print(temp);
-  Serial.println("*F ");
+  // Serial.print("FAHRENHEIT: ");
+  // Serial.print(temp);
+  // Serial.println("*F ");
+
+  return temp;
   
   }
